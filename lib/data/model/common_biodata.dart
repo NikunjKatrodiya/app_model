08e25e42f47155527ff3d06bbdb2f70e91a1b4f8
Nikunj Katrodiya <nikunjkatrodiya008@gmail.com>
2023-03-27 09:42:36 +0530

    update

--- lib/data/model/common_biodata.dart
+++ lib/data/model/common_biodata.dart
@@ -1,17 +1,21 @@
+import 'package:flutter/material.dart';
+
 class CommonBioData {
   final Info? info;
   final List<Results>? results;
 
   CommonBioData({this.info, this.results});
-  factory CommonBioData.fromJson(Map<String, dynamic> json){
+  factory CommonBioData.fromJson(Map<String, dynamic> json) {
     return CommonBioData(
-      results: List<Results>.from(json["results"].map((value) => Results.fromJson(value))),
+      results: List<Results>.from(
+          json["results"].map((value) => Results.fromJson(value))),
       info: Info.fromJson(json['info']),
     );
   }
 }
 
 class Results {
+  final Color? color;
   final int? id;
   final String? name;
   final String? status;
@@ -24,7 +28,8 @@ class Results {
   final List<String>? episode;
 
   Results(
-      {this.episode,
+      {this.color,
+      this.episode,
       this.image,
       this.id,
       this.name,
@@ -37,6 +42,7 @@ class Results {
 
   factory Results.fromJson(Map<String, dynamic> json) {
     return Results(
+      color: json['color'],
       id: json['id'],
       name: json['name'],
       status: json['status'],
@@ -48,10 +54,8 @@ class Results {
       origin: Origin.fromJson(json['origin']),
       location: Location.fromJson(json['location']),
     );
-
   }
 }
-
 
 class Location {
   final String? name;
@@ -88,7 +92,7 @@ class Info {
   final String? prev;
 
   Info({this.count, this.pages, this.next, this.prev});
-  factory Info.fromJson(Map<String,dynamic>json){
+  factory Info.fromJson(Map<String, dynamic> json) {
     return Info(
       count: json['count'],
       pages: json['pages'],
--- lib/screen/biodata_screen.dart
+++ lib/screen/biodata_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 import '../data/model/common_biodata.dart';
 import 'biodata.dart';
 
@@ -16,24 +15,74 @@ class _BioDataScreenState extends State<BioDataScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    commonBioData=CommonBioData.fromJson(BioData.bioList);
+    commonBioData = CommonBioData.fromJson(BioData.bioList);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("API Calling Screen"),
+        centerTitle: true,
+      ),
       body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
           child: Column(
-        children: [
-          Text("count: ${commonBioData!.info!.count}"),
-          Text("count: ${commonBioData!.info!.next}"),
-          Text("count: ${commonBioData!.info!.pages}"),
-          Text("count: ${commonBioData!.results![0].id}"),
-          Text("count: ${commonBioData!.results![1].name}"),
-          Image(image: NetworkImage("${commonBioData!.results![2].image})")),
-        ],
-      ),),
+            children: [
+              //Text("count: ${commonBioData!.info!.count}"),
+              //Text("count: ${commonBioData!.info!.next}"),
+              //Text("count: ${commonBioData!.info!.pages}"),
+              //Text("count: ${commonBioData!.results![0].id}"),
+              //Text("count: ${commonBioData!.results![1].name}"),
+              //Image(image: NetworkImage("${commonBioData!.results![2].image}")),
+              Expanded(
+                child: ListView.separated(
+                    itemBuilder: (context, index) => const Divider(),
+                    separatorBuilder: (context, index) => Container(
+                          height: 1350,
+                          width: 300,
+                          color: (commonBioData!.results![index].color),
+                          child: Column(
+                            children: [
+                              Text("count: ${commonBioData!.info!.count}"),
+                              Text("pages: ${commonBioData!.info!.pages}"),
+                              Text("next: ${commonBioData!.info!.next}"),
+                              Text("id: ${commonBioData!.results![index].id}"),
+                              Text(
+                                  "name: ${commonBioData!.results![index].name}"),
+                              Text(
+                                  "status: ${commonBioData!.results![index].status}"),
+                              Text(
+                                  "species: ${commonBioData!.results![index].species}"),
+                              Text(
+                                  "type: ${commonBioData!.results![index].type}"),
+                              Text(
+                                  "gender: ${commonBioData!.results![index].gender}"),
+                              Text(
+                                  "name: ${commonBioData!.results![index].origin!.name}"),
+                              Text(
+                                  "url: ${commonBioData!.results![index].origin!.url}"),
+                              Text(
+                                  "name: ${commonBioData!.results![index].location!.name}"),
+                              Text(
+                                  "url: ${commonBioData!.results![index].location!.url}"),
+                              Image.network(
+                                  "${commonBioData!.results![index].image}"),
+                              Text(
+                                  "episode: ${commonBioData!.results![index].episode}"),
+                            ],
+                          ),
+                        ),
+                    itemCount: commonBioData!.results!.length),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
